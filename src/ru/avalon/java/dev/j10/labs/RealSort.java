d5556abf939f3cbbcd08481e9a2715ab7c708b7a
Alex <61654925+LAV007@users.noreply.github.com>
2020-03-30 19:00:14 +0300

    Create RealSort.java

---
+++ src/ru/avalon/java/dev/j10/labs/RealSort.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.avalon.java.dev.j10.labs;
+
+import java.util.Comparator;
+
+/**
+ *
+ * @author vbg47
+ */
+public class RealSort implements Sort {
+
+    @Override
+    public void sort(Object[] array) {
+        
+    }
+
+    @Override
+    public void sort(Comparable[] array) {
+        
+    }
+
+    @Override
+    public void sort(Object[] array, Comparator comparator) {
+        
+    }
+    
+}
