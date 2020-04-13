6dcb88286c41a41d4e830452d01d84a7b877ddc0
Alex <61654925+LAV007@users.noreply.github.com>
2020-04-13 12:57:01 +0300

    Update RealPerson.java

--- src/ru/avalon/java/dev/j10/labs/RealPerson.java
+++ src/ru/avalon/java/dev/j10/labs/RealPerson.java
@@ -1,36 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ru.avalon.java.dev.j10.labs;
 
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.text.SimpleDateFormat;
 
-/**
- *
- * @author vbg47
- */
 public class RealPerson implements Person{
     
     private String name;
     private Date data;
     private GregorianCalendar calendar;
+    private SimpleDateFormat dateForm = null;
     
     public RealPerson(String name, int month, int day, int year){
         this.name = name;
-        calendar = new GregorianCalendar(year, month - 1/*ДЛЯ СЕБЯ - минус один - потому что массив!!!*/, day);
+        calendar = new GregorianCalendar(year, month - 1, day);
+        dateForm = new SimpleDateFormat("dd MMMM");
         data = calendar.getTime();
+        dateForm.format(data);        
     }
-          /*Когда создаю с помощью цикла for новые объекты типа RealPerson, не могу использовать этот конструктор, т.к.:
-          1.) не могу задать разные имена для разных объектов --- +++;
-          2.) не понял как работать с классом Date --- разобрался*/ 
-    
-    public void setName(String n){  /* <-- Почему не могу использовать этот метод в главном классе? persons[0].setName("");
-                                           Из-за того что implements, a не extends (Интерфейс, а не класс)?
-                                           Проповал решить этот вопрос с помощью static метода вне метода main - не помогло
-                                           ***!!!ОТВЕТ СМОТРИ В ТЛГ-КАНАЛЕ!!!***/
+       
+    public void setName(String n){
         name = n;
     }
 
@@ -46,9 +36,9 @@ public class RealPerson implements Person{
 
     @Override
     public int compareTo(Person t) {
-        if (this.hashCode() == t.hashCode()) {
+        if (this.name.length() == t.getName().length()) {
             return 0;
-        } else if (this.hashCode() < t.hashCode()) {
+        } else if (name.length() < t.getName().length()) {
             return -1;
         } else {
             return 1;
